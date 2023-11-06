bb7648006657ff0e876df890e5ccb93f408e94be
emore23 <estefani.muller.more@gmail.com>
2023-11-05 22:32:37 -0300

    feat: added company repository and creation

--- src/routes.ts
+++ src/routes.ts
@@ -3,14 +3,17 @@ import { Router } from "express";
 import { CreateUserController } from "./controllers/CreateUserController";
 import { LoginController } from "./controllers/LoginController";
 import { authMiddleware } from "./middlewares/authMiddleware";
+import { CreateCompanyController } from "./controllers/CreateCompanyController";
 
 const routes = Router();
 
 routes.post("/user", new CreateUserController().createUser);
 routes.post("/login", new LoginController().loginUser);
+routes.post("/company", new CreateCompanyController().createCompany);
 
 routes.use(authMiddleware);
 
+routes.get("/company", new CreateCompanyController().getCompany);
 routes.get("/profile", new LoginController().getProfile);
 
 export default routes;
